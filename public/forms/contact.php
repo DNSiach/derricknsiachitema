ac0723c1c4cc9bcd98d3fbe119e864f1c866c95d
DNSiach <derricknsiachitema@gmail.com>
2025-11-28 12:55:30 +0200

    Use Formspree for contact form; disable PHP handler and fix Vercel asset routes

--- public/forms/contact.php
+++ public/forms/contact.php
@@ -9,36 +9,9 @@
    // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'derricknsiachitema@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  if(isset($_POST['phone'])) {
-    $contact->add_message( $_POST['phone'], 'Phone');
-  }
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+  // This site uses Formspree for form handling on static hosts.
+  // If you still want to use the PHP library, restore the original code and
+  // place the php-email-form library at ../assets/vendor/php-email-form/php-email-form.php
+  http_response_code(410);
+  echo 'This endpoint is not used. The site is configured to use Formspree. Replace the Formspree action in the form if you need a different handler.';
 ?>
